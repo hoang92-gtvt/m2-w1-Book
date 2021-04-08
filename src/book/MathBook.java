c3c66679de0dc583693d171168e23110879c2717
hoang-gtvt <hoang92.gtvt@gmail.com>
2021-04-08 14:55:54 +0700

    'book'

---
+++ src/book/MathBook.java
@@ -0,0 +1,76 @@
+package book;
+
+public class MathBook {
+    String bookCode;
+    String name;
+    int price;
+    int number;
+    String outhor;
+    String language;
+    String fameWork;
+
+    //Khoi tao sach
+        public MathBook(String _name, int _number,int _price, String _language){
+            this.name= _name;
+            this.number= _number;
+            this.price= _price;
+            this.language = _language;
+        }
+
+    public MathBook() {
+    }
+
+    //Khoi tao phương thuc thuoc tinh
+    public void setBookCode(String _bookCode){
+        this.bookCode = _bookCode;
+    }
+    public String getBookCode(){
+        return this.bookCode;
+    };
+
+    public void setName(String _name){
+        this.name =_name;
+
+    }
+    public String getName(){
+        return this.name;
+    }
+    public void setPrice(int _price){
+        this.price= _price;
+    }
+    public int getPrice(){
+        return this.number;
+    }
+    public void setNumber(int _number){
+        this.number = _number;
+    }
+    public int getNumber(){
+        return this.number;
+    }
+
+    public void setOuthor(String _outhor){
+        this.outhor =_outhor;
+
+    }
+    public String getOuthor(){
+        return this.outhor;
+    }
+
+    public void setLanguage(String _language){
+        this.language = _language ;
+
+    }
+    public String getLang(){
+        return this.language;
+    }
+
+    public void setFameWork(String _fameWork){
+        this.fameWork = _fameWork;
+    }
+
+    //phương thức cần làm
+    public int getAmount(){
+        return this.price*this.number;
+    }
+
+}
---
+++ src/main/Main.java
@@ -0,0 +1,82 @@
+package main;
+import book.*;
+
+import java.util.Scanner;
+
+
+public class Main {
+
+//    private countBook(){
+        private int sumPrice(MathBook[] arr){
+            int sum =0;
+            for (int i = 0; i <arr.length ; i++) {
+                sum +=arr[i].getPrice();
+            }
+            return sum;
+        }
+        private int countBook( MathBook[] arr){
+            int count=0;
+            for (int i = 0; i < arr.length ; i++) {
+                if(arr[i].getLang()=="Java"){
+                    count++;
+                }
+            }
+            return count;
+        }
+        private void findbook(MathBook[] arr){
+            Scanner input=new Scanner(System.in);
+            System.out.println("Nhập giá tiền ");
+            int coat=input.nextInt();
+
+            for (int i = 0; i < arr.length; i++) {
+                if(arr[i].getPrice()>coat){
+                    System.out.println(arr[i].getName());
+                }
+            }
+        }
+        private String[] findbook2(MathBook[] arr){
+            Scanner input=new Scanner(System.in);
+            System.out.println("Nhập giá tiền ");
+            int coat=input.nextInt();
+            String[] arr2 =new String[arr.length];
+            for (int i = 0; i < arr.length; i++) {
+                if(arr[i].getPrice()>coat){
+                    arr2[i]=arr[i].getName();
+                }
+            }
+            return arr2;
+        }
+
+        public static void main(String[] args) {
+//        MathBook mathBooks[] = new MathBook[10];
+//        for (int i = 0; i <mathBooks.length ; i++) {
+//            String _name = "Toán"+i;
+//            mathBooks[i] = new MathBook();
+//
+//            mathBooks[i].setName(_name);
+//            mathBooks[i].setPrice(i*1000);
+//            mathBooks[i].setNumber(i*10);
+//
+//        }
+//        for (int i = 0; i < mathBooks.length ; i++) {
+//            System.out.println(mathBooks[i].getName());
+//            System.out.println(mathBooks[i].getAmount());
+//        }
+         MathBook[] mathbooks =new MathBook[10];
+         mathbooks[0]= new MathBook("mathbook1", 10, 10000, "Java")  ;
+         mathbooks[1]= new MathBook("mathbook2", 5, 12000, "Java")  ;
+         mathbooks[2]= new MathBook("mathbook3", 5, 13000, "Vietnam")  ;
+         mathbooks[3]= new MathBook("mathbook4", 7, 15000, "English")  ;
+         mathbooks[4]= new MathBook("mathbook5", 2, 15000, "Java")  ;
+         mathbooks[5]= new MathBook("mathbook6", 2, 15000, "Java")  ;
+         mathbooks[6]= new MathBook("mathbook7", 7, 15000, "English")  ;
+         mathbooks[7]= new MathBook("mathbook8", 2, 15000, "Java")  ;
+         mathbooks[8]= new MathBook("mathbook9", 7, 15000, "English")  ;
+         mathbooks[9]= new MathBook("mathbook10", 2, 15000, "Java")  ;
+
+            sumPrice(mathbooks);
+            countBook(mathbooks);
+            findbook(mathbooks);
+
+    }
+}
